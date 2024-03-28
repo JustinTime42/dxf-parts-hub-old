c5d022ef9ec3d193506451f863bc44ede8dc7e00
JustinTime42 <justincase18@msn.com>
2024-03-27 22:55:45 -0800

    testing python functions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+        destination: 'http://127.0.0.1:5328/api/:path*', // Proxy to Backend
       },
     ]
   },
