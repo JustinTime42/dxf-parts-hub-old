846fa804810e2989caed9e8aa7433ebc8b5cb00d
JustinTime42 <justincase18@msn.com>
2024-03-27 22:49:59 -0800

    testing python functions

--- src/app/shape-editor/[shape]/page.tsx
+++ src/app/shape-editor/[shape]/page.tsx
@@ -32,7 +32,7 @@ const ShapeEditor: React.FC<ShapeEditorProps> = ({ params }: { params: { shape: 
     console.log(numbers);
     e.preventDefault();
     try {
-      await fetch(`http://127.0.0.1:5328/api/generate_gasket`, {
+      await fetch(`/api/generate_gasket`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
